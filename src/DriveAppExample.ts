ad5b8069fd1f9097d6dd5a70b1b5eeab5af2d7e8
sumeragizzz <developer@sumeragizzz.dev>
2019-08-11 22:54:12 +0900

    add DriveApp sample

--- src/DriveAppExample.ts
+++ src/DriveAppExample.ts
@@ -1,3 +1,14 @@
 function main() {
+    const sampleFile = DriveApp.getFileById(PropertiesService.getScriptProperties().getProperty('SAMPLE_FILE_ID'));
+    const sampleFolder = DriveApp.getFolderById(PropertiesService.getScriptProperties().getProperty('SAMPLE_FOLDER_ID'));
+    Logger.log(sampleFile.getName());
+    Logger.log(sampleFolder.getName());
 
+    // 指定したファイルをDriveのルートフォルダに追加(元のファルダにも属したまま)
+    const addFileResult = DriveApp.addFile(sampleFile);
+    Logger.log(addFileResult.getName());
+
+    // 指定したフォルダをDriveのルートフォルダに追加(元のファルダにも属したまま)
+    const addFolderResult = DriveApp.addFolder(sampleFolder);
+    Logger.log(addFolderResult.getName());
 }
