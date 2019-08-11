cecf532758a682dac55e4287e3f99467712bcb54
sumeragizzz <developer@sumeragizzz.dev>
2019-08-12 00:33:06 +0900

    add DriveApp sample

--- src/DriveAppExample.ts
+++ src/DriveAppExample.ts
@@ -4,11 +4,11 @@ function main() {
     Logger.log(sampleFile.getName());
     Logger.log(sampleFolder.getName());
 
-    // 指定したファイルをDriveのルートフォルダに追加(元のファルダにも属したまま)
+    // 指定したファイルをDriveのルートフォルダに追加。元のファルダにも属したまま。
     const addFileResult = DriveApp.addFile(sampleFile);
     Logger.log(addFileResult.getName());
 
-    // 指定したフォルダをDriveのルートフォルダに追加(元のファルダにも属したまま)
+    // 指定したフォルダをDriveのルートフォルダに追加。元のファルダにも属したまま。
     const addFolderResult = DriveApp.addFolder(sampleFolder);
     Logger.log(addFolderResult.getName());
 
@@ -18,11 +18,15 @@ function main() {
 
     // createFile(blob)
 
-    // 新規テキストファイル作成
+    // 新規テキストファイル作成。ルートに配置される。
     const createFileResult2 = DriveApp.createFile('Sample.txt', 'create file result A');
     Logger.log(createFileResult2.getName());
 
-    // MIMEタイプ指定で新規ファイル作成
+    // MIMEタイプ指定で新規ファイル作成。ルートに配置される。
     const createFileResult3 = DriveApp.createFile('Sample.html', '<ul><li>A</li><li>B</li><li>C</li></ul>', 'text/html');
     Logger.log(createFileResult3.getName());
+
+    // 新規フォルダ作成。ルートに配置される。
+    const createFolderResult = DriveApp.createFolder('CreatedFolder');
+    Logger.log(createFolderResult.getName());
 }
