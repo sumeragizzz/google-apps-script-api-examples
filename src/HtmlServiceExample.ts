020906ca54d2d78474c3c3e6776a045b4d7251db
sumeragizzz <developer@sumeragizzz.dev>
2019-08-23 23:27:47 +0900

    add HtmlService sample

---
+++ src/HtmlServiceExample.ts
@@ -0,0 +1,7 @@
+function doGet(e:GoogleAppsScript.Events.AppsScriptHttpRequestEvent) {
+    return HtmlService.createHtmlOutputFromFile('index');
+}
+
+function doPost(e:GoogleAppsScript.Events.AppsScriptHttpRequestEvent) {
+
+}
