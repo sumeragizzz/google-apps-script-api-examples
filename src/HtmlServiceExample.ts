2d3fda7783606795913cc318198bb61ceb903461
sumeragizzz <developer@sumeragizzz.dev>
2019-08-24 21:53:56 +0900

    add HtmlService sample

--- src/HtmlServiceExample.ts
+++ src/HtmlServiceExample.ts
@@ -1,7 +1,14 @@
 function doGet(e:GoogleAppsScript.Events.AppsScriptHttpRequestEvent) {
-    return HtmlService.createHtmlOutputFromFile('index');
+    return HtmlService.createTemplateFromFile('index').evaluate();
+    // return HtmlService.createHtmlOutputFromFile('index');
 }
 
 function doPost(e:GoogleAppsScript.Events.AppsScriptHttpRequestEvent) {
+    const parameter: any = e.parameter;
 
+    const template = HtmlService.createTemplateFromFile('index');
+    template.keyResult = parameter.key;
+    template.valueResult = parameter.value;
+
+    return template.evaluate();
 }
